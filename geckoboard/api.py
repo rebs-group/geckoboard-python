39f51bc04f1172bb1c7ec1c4bcd84785af38299c
Felix Kerekes <felix@rebs-group.com>
2017-11-02 16:59:59 +0200

    Use http_requests

--- geckoboard/api.py
+++ geckoboard/api.py
@@ -2,22 +2,22 @@
 Private: api access methods
 """
 
-import requests
+import http_requests
 
 API_HOST = 'https://api.geckoboard.com'
 
 
 def get(path, api_key):
-    return requests.get(API_HOST + path, auth=(api_key, ''))
+    return http_requests.get(API_HOST + path, auth=(api_key, ''))
 
 
 def delete(path, api_key):
-    return requests.delete(API_HOST + path, auth=(api_key, ''))
+    return http_requests.delete(API_HOST + path, auth=(api_key, ''))
 
 
 def post(path, body, api_key):
-    return requests.post(API_HOST + path, json=body, auth=(api_key, ''))
+    return http_requests.post(API_HOST + path, json=body, auth=(api_key, ''))
 
 
 def put(path, body, api_key):
-    return requests.put(API_HOST + path, json=body, auth=(api_key, ''))
+    return http_requests.put(API_HOST + path, json=body, auth=(api_key, ''))
